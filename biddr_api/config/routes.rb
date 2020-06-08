922dc65d6ede60f34ecfccf7f5aaba64a229ed96
Selim Ozdogan <selimozdogan@hotmail.com>
2020-06-08 12:18:39 -0700

    added session create and user_info

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/users/users_info" => "users#users_info"
-  resource :session, only: [:new, :create, :destroy]
-
-
+  
+  
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :auctions
+      resource :session, only: [:new, :create, :destroy]
       
     end
   end
