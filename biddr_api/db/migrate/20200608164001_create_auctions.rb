7d00a4df0589ffdcd7028608437a6e2ca6f85313
Selim Ozdogan <selimozdogan@hotmail.com>
2020-06-08 10:11:06 -0700

    added auction migration

--- biddr_api/db/migrate/20200608164001_create_auctions.rb
+++ biddr_api/db/migrate/20200608164001_create_auctions.rb
@@ -1,7 +1,10 @@
 class CreateAuctions < ActiveRecord::Migration[6.0]
   def change
     create_table :auctions do |t|
-
+      t.string :title
+      t.text :description
+      t.timestamp :ends_at
+      t.integer :reverve_price
       t.timestamps
     end
   end
